0861d3dc45a8e8f6cc24560fb049b0bd95058efa
viveralia <edgarv10@gmail.com>
2019-10-16 20:12:12 -0500

    changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ const index = () => {
     <div className="container my-5">
       <div className="row">
         <div className="col-12">
-          <h1>Hello from Gatsby</h1>
+          <h1>Hello from changes</h1>
           <Link to="/about">Go to page 2</Link>
         </div>
       </div>
